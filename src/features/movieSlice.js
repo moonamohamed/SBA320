d369ec11883b6c6847451a2ef36feafa6f531851
mmoha.Muna <mmoha.munamohamed2890@gmail.com>
2025-07-08 11:45:27 -0400

    third commit

--- src/features/movieSlice.js
+++ src/features/movieSlice.js
@@ -2,3 +2,13 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 
+const API_KEY = import.meta.env.vite_OMDB_API_KEY;
+
+export const fetchMovies = createAsyncThunk(
+    'movie/fetchMovies',
+    async (searchTerm) => {
+        const res = await.get('https://www.omdbapi.com/?apikey=${API_KEY}&s=${searchTerm}');
+        return res.data.Search || [];
+    }
+);
+
