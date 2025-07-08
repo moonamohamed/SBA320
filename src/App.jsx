f0b00ee1216d95b11e3d26d6fb8e9d771c4fd5b6
mmoha.Muna <mmoha.munamohamed2890@gmail.com>
2025-07-08 11:22:35 -0400

    added code

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMovies } from 
+
+import { useState } from 'react'
+
+
+import './App.css'
+
+function movie-app() {
+  const [count, setCount] = useState(0)
+}
+
+  return ()
+    
+export default movie-app
