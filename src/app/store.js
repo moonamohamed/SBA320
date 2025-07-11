c49df66f6fe3ce70178b07f0e79a735780ef1d1d
mmoha.Muna <mmoha.munamohamed2890@gmail.com>
2025-07-11 19:56:57 -0400

    updated code

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import movieReducer from '../features/movieSlice';
+import jokesReducer from '../features/jokesSliceSlice';
 
 export const store = configureStore({
     reducer: {
-        movie: movieReducer
-    }
+        joke: jokesReducer,
+    },
 });
