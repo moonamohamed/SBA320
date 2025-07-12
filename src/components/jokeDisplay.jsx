abecc42e6b9cfd41c43814e806e692f7889697a6
mmoha.Muna <mmoha.munamohamed2890@gmail.com>
2025-07-11 20:13:15 -0400

    added code

--- src/components/jokeDisplay.jsx
+++ src/components/jokeDisplay.jsx
@@ -5,5 +5,16 @@ export default function JokeDisplay() {
     const dispatch = useDispatch();
     const { joke, status } = useSelector((state) => state.joke);
 
-    
+    const handClick = () => {
+        dispatch(fetchJoke());
+    };
+
+    return(
+        <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+            <h1>Random Jokes</h1>
+            <button onClick={handClick}>Tell Me a Joke</button>
+            {status === 'loading' && <p>Loading...</p>}
+            {joke && <p id='displayJoke' style={{ marginTop: '1rem' }}>{joke}</p>}
+        </div>
+    );
 }
